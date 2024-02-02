36fe2fcd23cea9e27934027650af4cdf2a691408
neallseth <32910906+neallseth@users.noreply.github.com>
2024-02-02 14:36:50 -0800

    fixing dropdown close / auction modal issue

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -8,6 +8,8 @@ import { LogIn, Server } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { SignInModal } from "./SignInModal";
 import { Tables } from "@/lib/types/database.types";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -37,7 +39,7 @@ import {
   PanelBottomClose,
 } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { BidClose } from "@/components/BidClose";
+import { CloseAuction } from "@/components/CloseAuction";
 
 type Props = {
   user: Tables<"users"> | null;
@@ -48,6 +50,7 @@ export default function AuthButton({ user, setUser }: Props) {
   const signOut = async () => {};
   const [modalOpen, setModalOpen] = useState(false);
   const [users, setUsers] = useState<Array<Tables<"users">>>([]);
+  const [dialogOpen, setDialogOpen] = useState(false);
 
   useEffect(() => {
     async function loadUsers() {
@@ -68,90 +71,102 @@ export default function AuthButton({ user, setUser }: Props) {
   }
 
   return user ? (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <div className="flex">
-          <Button variant={"ghost"}>
-            {" "}
-            <Avatar className="w-6 h-6 mr-2">
-              <AvatarImage
-                src={user.name === "Neall Seth" ? "./assets/yud.jpg" : ""}
-              />
-              <AvatarFallback>{user.name?.split(" ")[0][0]}</AvatarFallback>
-            </Avatar>
-            {user.name?.split(" ")[0]}
-          </Button>
-        </div>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
-        <DropdownMenuLabel>{user.name}</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem disabled>
-            <User className="mr-2 h-4 w-4" />
-            <span>My account</span>
-            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem disabled>
-            <CreditCard className="mr-2 h-4 w-4" />
-            <span>Billing</span>
-            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem disabled>
-            <History className="mr-2 h-4 w-4" />
-            <span>Order history</span>
-            <DropdownMenuShortcut>⌘H</DropdownMenuShortcut>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem disabled>
-          <Server className="mr-2 h-4 w-4" />
-          <span>Capacity info</span>
-        </DropdownMenuItem>
-        <DropdownMenuItem disabled>
-          <LifeBuoy className="mr-2 h-4 w-4" />
-          <span>Support</span>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem disabled>
-          <Settings className="mr-2 h-4 w-4" />
-          <span>Settings</span>
-          <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-        </DropdownMenuItem>
-        <DropdownMenuSub>
-          <BidClose>
-            <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
-              <PanelBottomClose className="mr-2 h-4 w-4" />
-              <span>Close auction</span>
+    <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <div className="flex">
+            <Button variant={"ghost"}>
+              {" "}
+              <Avatar className="w-6 h-6 mr-2">
+                <AvatarImage
+                  src={user.name === "Neall Seth" ? "./assets/yud.jpg" : ""}
+                />
+                <AvatarFallback>{user.name?.split(" ")[0][0]}</AvatarFallback>
+              </Avatar>
+              {user.name?.split(" ")[0]}
+            </Button>
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>{user.name}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem disabled>
+              <User className="mr-2 h-4 w-4" />
+              <span>My account</span>
+              <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
             </DropdownMenuItem>
-          </BidClose>
-          <DropdownMenuSubTrigger>
-            <UserPlus className="mr-2 h-4 w-4" />
-            <span>Change user</span>
-          </DropdownMenuSubTrigger>
-          <DropdownMenuPortal>
-            <DropdownMenuSubContent>
-              {users.map((user) => {
-                return (
-                  <DropdownMenuItem
-                    key={user.id}
-                    onClick={() => handleSignIn(user)}
-                  >
-                    <User className="mr-2 h-4 w-4" />
-                    <span>{user.name}</span>
-                  </DropdownMenuItem>
-                );
-              })}
-            </DropdownMenuSubContent>
-          </DropdownMenuPortal>
-        </DropdownMenuSub>
+            <DropdownMenuItem disabled>
+              <CreditCard className="mr-2 h-4 w-4" />
+              <span>Billing</span>
+              <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuItem disabled>
+              <History className="mr-2 h-4 w-4" />
+              <span>Order history</span>
+              <DropdownMenuShortcut>⌘H</DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem disabled>
+            <Server className="mr-2 h-4 w-4" />
+            <span>Capacity info</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem disabled>
+            <LifeBuoy className="mr-2 h-4 w-4" />
+            <span>Support</span>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem disabled>
+            <Settings className="mr-2 h-4 w-4" />
+            <span>Settings</span>
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuSub>
+            <DialogTrigger asChild>
+              <DropdownMenuItem
+              // onSelect={(e) => {
+              //   e.preventDefault();
+              //   setDropdownOpen(false);
+              // }}
+              >
+                <PanelBottomClose className="mr-2 h-4 w-4" />
+                <span>Close auction</span>
+              </DropdownMenuItem>
+            </DialogTrigger>
+            <DropdownMenuSubTrigger>
+              <UserPlus className="mr-2 h-4 w-4" />
+              <span>Change user</span>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                {users.map((user) => {
+                  return (
+                    <DropdownMenuItem
+                      key={user.id}
+                      onClick={() => handleSignIn(user)}
+                    >
+                      <User className="mr-2 h-4 w-4" />
+                      <span>{user.name}</span>
+                    </DropdownMenuItem>
+                  );
+                })}
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
 
-        <DropdownMenuItem onClick={() => setUser(null)}>
-          <LogOut className="mr-2 h-4 w-4" />
-          <span>Log out</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <DropdownMenuItem onClick={() => setUser(null)}>
+            <LogOut className="mr-2 h-4 w-4" />
+            <span>Log out</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <CloseAuction
+        closeBidModal={() => {
+          setDialogOpen(false);
+        }}
+      />
+    </Dialog>
   ) : (
     <SignInModal
       modalOpen={modalOpen}
