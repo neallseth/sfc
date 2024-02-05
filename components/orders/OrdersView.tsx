cdf2a99fea10313377fb3a0f23861112c29aa4df
neallseth <32910906+neallseth@users.noreply.github.com>
2024-02-05 14:27:35 -0800

    order view styling

--- components/orders/OrdersView.tsx
+++ components/orders/OrdersView.tsx
@@ -40,12 +40,7 @@ export default function OrdersView({
   if ((!user && userLoading) || bidsLoading) {
     return (
       <div className="flex flex-col gap-2 grow">
-        <div className="flex gap-2">
-          <Skeleton className="h-16 w-1/3" />
-          <Skeleton className="h-16 w-1/3" />
-          <Skeleton className="h-16 w-1/3" />
-        </div>
-        <Skeleton className="h-48" />
+        <Skeleton className="h-64" />
       </div>
     );
   } else if (user) {
